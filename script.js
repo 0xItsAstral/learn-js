0f6f4821f25793795d97840f126fea6fba544ca5
Muhammad Asgarul Tsani <muhammadasgarultsani@gmail.com>
2025-06-08 07:46:19 +0800

    data string

---
+++ script.js
@@ -0,0 +1,16 @@
+document.writeln("<h1>hello world</h1>"); // Menulis text di body
+
+
+/* data string
+\n = enter
+\t = tab
+\" = % \' "&'
+\\ = \
+*/
+
+document.writeln("<textarea cols= '100' rows= '10'>")
+document.write("hello \n") 
+document.write("\"its astral\"\n")
+document.write("\tbelajar javascript\n")
+document.write("\\github\\0xitsAstral")
+document.writeln("</textarea>")
